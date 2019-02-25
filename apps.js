efd3fb8ce70fa04b25d313176adbe35226d9200a
cusimanog <47872723+cusimanog@users.noreply.github.com>
2019-02-25 14:50:44 -0800

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,7 +1,7 @@
-const navSlide=()=>{
+const navSlide = () =>{
     const burger = document.querySelectorelector('.burger');
-    const nav= document.querySelector('.page-nav-links');
-    
+    const nav = document.querySelector('.page-nav-links');
+ 
     burger.addeventlistener('click',()=>{
         nav.classList.toggle('page-nav-active');
     });
