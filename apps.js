6b035f540ea593a2358708ad6f49876145246b67
cusimanog <47872723+cusimanog@users.noreply.github.com>
2019-02-26 10:34:55 -0800

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,9 +1,25 @@
-const navSlide = () =>{
-    const burger = document.querySelectorelector('.burger');
-    const nav = document.querySelector('.page-nav-links');
- 
-    burger.addeventlistener('click',()=>{
-        nav.classList.toggle('page-nav-active');
-    });
+const pagenavSlide = () => {
+    const burger = document.querySelector('.burger');
+    const pagenav = document.querySelector('.pagenav-links');
+    const pagenavLinks = document.querySelectorAll('.pagenav-links li');
+    
+    
+    burger.addEventListener('click', ()=> {
+        //Toggle Pagenav
+        pagenav.classList.toggle('pagenav-active');
+       
+        //Animate Links
+        pagenavLinks.forEach((link,index)=> {
+            if(link.style.animtion){
+                link.style.animation = '';
+            } else {
+                link.style.animation = `pagenavLinkFade .5s ease forwards ${index / 7+1}s`;
+                }
+            });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+        });
+    
 }
-navSlide();
+
+pagenavSlide();
